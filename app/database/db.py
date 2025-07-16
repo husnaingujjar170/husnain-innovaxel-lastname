c8d0b5eac45c4080c4939309308f874fdc54cb57
Husnain Mahmood <husnain.mahmood2002@gmail.com>
2025-07-17 00:31:10 +0500

    I have connected to mongodb  database.

--- app/database/db.py
+++ app/database/db.py
@@ -4,3 +4,13 @@ from fastapi import FastAPI
 client = None
 db = None
 
+async def init_db(app: FastAPI):
+    global client, db
+    client = AsyncIOMotorClient("mongodb:://localhost:2701")
+    db = client["url_shortener"]
+    print("MongoDb connected")
+
+async def close_db(app: FastAPI):
+    client.close()
+    print("MongoDB connection closed")
+
