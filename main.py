9a13ca983198a097aea36123b070919b8ce61732
Husnain Mahmood <husnain.mahmood2002@gmail.com>
2025-07-17 22:15:02 +0500

    Url Response and Config classes also defined

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import os
 from pydantic import BaseModel, HttpUrl
-
+from datetime import datetime
+from bson import ObjectId
 app = FastAPI
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
@@ -9,3 +10,21 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
 class UrlCreate(BaseModel):
     original_url: HttpUrl
+
+class UrlUpdate(BaseModel):
+    original_url: HttpUrl
+
+class UrlResponse(BaseModel):
+    short_code: str
+    original_url: str
+    created_at: datetime
+    access_count: int
+    access_times: list[datetime]
+
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.isoformat(),
+            ObjectId: str
+        }
+
+
